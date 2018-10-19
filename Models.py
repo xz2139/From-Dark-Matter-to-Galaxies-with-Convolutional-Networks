c9c024b1727afee795ad2133aac033b3688f9f60
Yueqiu <ys3202>
2018-10-18 22:39:24 -0400

    classification trainer

--- Models.py
+++ Models.py
@@ -43,39 +43,110 @@ class SimpleUnet(nn.Module):
         
         super(SimpleUnet, self).__init__()
         self.in_channels = in_channels
-        self.Simple_Unet = nn.Sequential(
-            self.conv_layer(self.in_channels, 16),
-            self.conv_layer(16, 16),
-            nn.AvgPool3d(2),
-            self.conv_layer(16,32),
-            nn.AvgPool3d(2),
-            self.conv_layer(32,64),
-            self.up_conv_layer(64, 64, 3),
-            self.conv_layer(64, 32),
-            self.up_conv_layer(32, 32, 3),
-            self.conv_layer(32, 16),
-            self.up_conv_layer(16, 16, 3),
-            self.conv_layer(16, 8),
-            self.up_conv_layer(8, 8, 3),
-            self.conv_layer(8, 4),
-            self.conv_layer(4, 1)
-        )
+        self.conv1=nn.Conv3d(self.in_channels, 16, kernel_size=3, stride=1, padding=0, bias=True)
+        self.conv2=nn.Conv3d(16, 16, kernel_size=3, stride=1, padding=0, bias=True)
+        self.pool1=nn.AvgPool3d(2)
+        self.conv3=nn.Conv3d(16, 32, kernel_size=3, stride=1, padding=0, bias=True)
+        self.pool2=nn.AvgPool3d(2)
+        self.conv4=nn.Conv3d(32, 64, kernel_size=3, stride=1, padding=0, bias=True)
+#         self.upsamp1= nn.Upsample(scale_factor=2, mode='nearest')
+#         self.upconv1=nn.Conv3d(64, 64, kernel_size=3, stride=1, padding=1, bias=False)        
+#         self.conv5=nn.Conv3d(64, 32, kernel_size=3, stride=1, padding=0, bias=True)
+#         self.upsamp2= nn.Upsample(scale_factor=2, mode='nearest')
+#         self.upconv2=nn.Conv3d(32, 32, kernel_size=3, stride=1, padding=1, bias=False)
+#         self.conv6=nn.Conv3d(32, 16, kernel_size=3, stride=1, padding=0, bias=True)
+#         self.upsamp3= nn.Upsample(scale_factor=2, mode='nearest')
+#         self.upconv3=nn.Conv3d(16, 16, kernel_size=3, stride=1, padding=1, bias=False)
+#         self.conv7=nn.Conv3d(16, 8, kernel_size=3, stride=1, padding=0, bias=True)
+#         self.upsamp4= nn.Upsample(scale_factor=2, mode='nearest')
+#         self.upconv4=nn.Conv3d(8, 8, kernel_size=3, stride=1, padding=1, bias=False)
+#         self.conv8=nn.Conv3d(8, 4, kernel_size=3, stride=1, padding=0, bias=True)
+#         self.conv9=nn.Conv3d(4, 1, kernel_size=3, stride=1, padding=0, bias=True)
+        self.relu=nn.ReLU()
+#         self.fc1=nn.Linear(43904,32768)
+#         self.fc2=nn.Linear(6912, 32768)
+        self.fc3=nn.Linear(4096,32768*3)
+#         self.Simple_Unet = nn.Sequential(
+#             self.conv_layer(self.in_channels, 16),
+#             self.conv_layer(16, 16),
+#             nn.AvgPool3d(2),
+#             self.conv_layer(16,32),
+#             nn.AvgPool3d(2),
+#             self.conv_layer(32,64),
+#             self.up_conv_layer(64, 64, 3),
+#             self.conv_layer(64, 32),
+#             self.up_conv_layer(32, 32, 3),
+#             self.conv_layer(32, 16),
+#             self.up_conv_layer(16, 16, 3),
+#             self.conv_layer(16, 8),
+#             self.up_conv_layer(8, 8, 3),
+#             self.conv_layer(8, 4),
+#             self.conv_layer(4, 1)
+#         )
     
-    def conv_layer(self, in_channels, out_channels, kernel_size=3, stride=1, padding=0, bias=True):
-        layers = nn.Sequential(
-        nn.Conv3d(in_channels, out_channels, kernel_size, stride=stride, padding=padding, bias=bias),
-        # nn.BatchNorm3d(out_channels),
-        nn.ReLU())
-        return layers
+#     def conv_layer(self, inputs, in_channels, out_channels, kernel_size=3, stride=1, padding=0, bias=True):
+# #         layers = nn.Sequential(
+#         nn.Conv3d(in_channels, out_channels, kernel_size, stride=stride, padding=padding, bias=bias)
+#         # nn.BatchNorm3d(out_channels),
+#         nn.ReLU()
+# #         print(layers.size())
+#         return layers
     
-    def up_conv_layer(self, in_channels, out_channels, kernel_size, stride=2, padding=1, output_padding=1, bias=True):
-        layers = nn.Sequential(
-        nn.ConvTranspose3d(in_channels,out_channels, kernel_size=kernel_size, stride=stride, padding=padding,output_padding=output_padding),
-#       nn.BatchNorm3d(out_channels),
-        nn.ReLU())
-        return layers
+#     def up_conv_layer(self, in_channels, out_channels, kernel_size, stride=1, padding=0, output_padding=0, bias=True):
+#         layers = nn.Sequential(
+#             nn.Upsample(scale_factor=2, mode='nearest'),
+#             # should be feat_in*2 or feat_in
+#             nn.Conv3d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False),
+#             # nn.BatchNorm3d(out_channels),
+#             nn.ReLU())
+#         return layers
     
     
     def forward(self, cube):
-        cube = self.Simple_Unet(cube)
+        b_size=cube.size()[0]
+        cube = self.conv1(cube)
+        cube = self.relu(cube)
+#         print(cube.size())
+        cube = self.conv2(cube)
+        cube = self.relu(cube)
+#         print(cube.size())
+        cube = self.pool1(cube)
+#         print(cube.size())
+#         linear1=self.fc1(cube.view(b_size,-1))
+        cube = self.conv3(cube)
+        cube = self.relu(cube)
+#         print(cube.size())
+        cube = self.pool2(cube)
+#         print(cube.size())
+#         linear2=self.fc2(cube.view(b_size,-1))
+        cube = self.conv4(cube)
+        cube = self.relu(cube)
+#         print(cube.size())
+        linear=self.fc3(cube.view(b_size,-1))
+#         linear=linear1+linear2+linear
+        #print('linear: ', linear.size())
+        cube=linear.reshape([b_size,3,32,32,32])
+#         cube = self.relu(cube)
+#         cube = self.upsamp1(cube)
+#         cube = self.upconv1(cube)
+#         cube = self.relu(cube)
+#         cube = self.conv5(cube)
+#         cube = self.relu(cube)
+#         cube = self.upsamp2(cube)
+#         cube = self.upconv2(cube)
+#         cube = self.relu(cube)
+#         cube = self.conv6(cube)
+#         cube = self.relu(cube)
+#         cube = self.upsamp3(cube)
+#         cube = self.upconv3(cube)
+#         cube = self.relu(cube)        
+#         cube = self.conv7(cube)
+#         cube = self.relu(cube)
+#         cube = self.upsamp4(cube)
+#         cube = self.upconv4(cube)
+#         cube = self.relu(cube) 
+#         cube = self.conv8(cube)
+#         cube = self.relu(cube)
+#         cube = self.conv9(cube)
+#         cube = self.relu(cube)
         return cube
--- main.py
+++ main.py
@@ -10,6 +10,8 @@ import torch.nn as nn
 import torch.backends.cudnn as cudnn
 import torch.optim
 import torch.utils.data
+import torch.nn.functional as F
+
 # import torch.utils.data.distributed
 # import torchvision.transforms as transforms
 # import torchvision.datasets as datasets
@@ -52,8 +54,10 @@ def parse_args():
 def initial_loss(train_loader, val_loader, model, criterion):
     batch_time = AverageMeter()
     train_losses = AverageMeter()
-    val_losses = AverageMeter()
-
+    losses = AverageMeter()
+    correct = 0
+    correct_1 = 0
+    total = 0
     # switch to train mode
     model.eval()
     
@@ -61,11 +65,14 @@ def initial_loss(train_loader, val_loader, model, criterion):
         for i, (input, target) in enumerate(train_loader):
             # add a dimension, from (1, 32, 32, 32) to (1,1,32,32,32)
             input = input.unsqueeze(dim = 1).to(device).float()
-            target = target.unsqueeze(dim = 1).to(device).float()
+            #target = target.unsqueeze(dim = 1).to(device).float()
+            target = target.to(device).long()
             # compute output
             output = model(input)
-            
+            # print("target1: ", target.size())
+            # print("output: ", output.size())
             loss = criterion(output, target)
+            #print(loss)
             # measure accuracy and record loss
             train_losses.update(loss.item(), input.size(0))
 
@@ -73,18 +80,34 @@ def initial_loss(train_loader, val_loader, model, criterion):
             # add a dimension, from (1, 32, 32, 32) to (1,1,32,32,32)
 
             input = input.unsqueeze(dim = 1).to(device).float()
-            target = target.unsqueeze(dim = 1).to(device).float()
+            #target = target.unsqueeze(dim = 1).to(device).float()
+            target = target.to(device).long()
             # compute output
             output = model(input)
             
             loss = criterion(output, target)
             # measure accuracy and record loss
-            val_losses.update(loss.item(), input.size(0))
+            losses.update(loss.item(), input.size(0))
+            outputs = F.softmax(output, dim=1)
+            predicted = outputs.max(1, keepdim=True)[1]
+            total += np.prod(target.shape)
+            correct += predicted.eq(target.view_as(predicted)).sum().item()
+            #correct_1 += torch.mul(predicted.eq(target.view_as(predicted)),(target >= 1)).sum().item()
+#             output=0*output
+            loss = criterion(output, target)
+            # measure accuracy and record loss
+            losses.update(loss.item(), input.size(0))
 
+            # measure elapsed time
+            #batch_time.update(time.time() - end)
+            #end = time.time()
+
+            
+    print('Inital Test: Loss {loss.avg:.4f} Accuracy {ac:.4f}\t'.format(loss=losses,ac=correct/total*100))
 
         
-        print('Training initial Loss {train_loss.avg:.4f}\t'
-        	'Validation initial Loss {val_loss.avg:.4f}\t'.format(train_loss=train_losses, val_loss = val_losses))
+        # print('Training initial Loss {train_loss.avg:.4f}\t'
+        # 	'Validation initial Loss {val_loss.avg:.4f}\t'.format(train_loss=train_losses, val_loss = val_losses))
 
 
 
@@ -106,9 +129,11 @@ def train(train_loader, model, criterion, optimizer, epoch):
 
         # add a dimension, from (1, 32, 32, 32) to (1,1,32,32,32)
         input = input.unsqueeze(dim = 1).to(device).float()
-        target = target.unsqueeze(dim = 1).to(device).float()
+        #target = target.unsqueeze(dim = 1).to(device).float()
+        target = target.to(device).long()
         # compute output
         output = model(input)
+
         #print(torch.nonzero(target).size())
         loss = criterion(output, target)
         # measure accuracy and record loss
@@ -134,23 +159,57 @@ def train(train_loader, model, criterion, optimizer, epoch):
     print('Epoch Train: Loss {loss.avg:.4f}\t'.format(loss=losses))
 
 def validate(val_loader, model, criterion):
+    # batch_time = AverageMeter()
+    # losses = AverageMeter()
+
+
+    # # switch to evaluate mode
+    
+    # model.eval()
+    # with torch.no_grad():
+    #     end = time.time()
+    #     for i, (input, target) in enumerate(val_loader):
+    #         input = input.unsqueeze(dim = 1).to(device).float()
+    #         #target = target.unsqueeze(dim = 1).to(device).float()
+    #         target = target.to(device).to(device).long()
+            
+    #         # compute output
+    #         output = model(input)
+            
+    #         loss = criterion(output, target)
+    #         # measure accuracy and record loss
+    #         losses.update(loss.item(), input.size(0))
+
+    #         # measure elapsed time
+    #         batch_time.update(time.time() - end)
+    #         end = time.time()
+
+            
+    #print('Epoch Test: Loss {loss.avg:.4f}\t'.format(loss=losses))
+
     batch_time = AverageMeter()
     losses = AverageMeter()
-
-
-    # switch to evaluate mode
-    
+    # switch to evaluate mode  
+    correct = 0
+    correct_1 = 0
+    total = 0
     model.eval()
     with torch.no_grad():
         end = time.time()
         for i, (input, target) in enumerate(val_loader):
             input = input.unsqueeze(dim = 1).to(device).float()
-            target = target.unsqueeze(dim = 1).to(device).float()
+#             target = target.unsqueeze(dim = 1).to(device).float()
+            target = target.to(device).long()
             
             
             # compute output
             output = model(input)
-            
+            outputs = F.softmax(output, dim=1)
+            predicted = outputs.max(1, keepdim=True)[1]
+            total += np.prod(target.shape)
+            correct += predicted.eq(target.view_as(predicted)).sum().item()
+            #correct_1 += torch.mul(predicted.eq(target.view_as(predicted)),(target >= 1)).sum().item()
+#             output=0*output
             loss = criterion(output, target)
             # measure accuracy and record loss
             losses.update(loss.item(), input.size(0))
@@ -160,7 +219,8 @@ def validate(val_loader, model, criterion):
             end = time.time()
 
             
-    print('Epoch Test: Loss {loss.avg:.4f}\t'.format(loss=losses))
+    print('Epoch Test: Loss {loss.avg:.4f} Accuracy {ac:.4f}\t'.format(loss=losses,ac=correct/total*100))
+    #print('Epoch Test: Loss {loss.avg:.4f} Accuracy {ac:.4f}  Recall {recall:.4f}\t'.format(loss=losses,ac=correct/total*100, ))
 
 def main():
 
@@ -217,7 +277,8 @@ def main():
         model = SimpleUnet(dim).to(device)
     elif model_idx == 1:
         model = Baseline(dim, dim).to(device)
-    criterion = nn.MSELoss().to(device) #yueqiu
+    #criterion = nn.MSELoss().to(device) #yueqiu
+    criterion = nn.CrossEntropyLoss().to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr,
                                     weight_decay=args.weight_decay)
     initial_loss(training_generator, validation_generator, model, criterion)
