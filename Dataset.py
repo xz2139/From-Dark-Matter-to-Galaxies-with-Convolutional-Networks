b20954dd06dbea46803858d344eb13717405fb43
Xinyue Zhang <xz2139@nyu.edu>
2018-10-30 15:32:49 -0400

    Update Dataset.py
    Add normalization on input

--- Dataset.py
+++ Dataset.py
@@ -14,5 +14,7 @@ class Dataset(data.Dataset):
         # Select sample
         ID = self.IDs[index]
         d_box=np.load('/scratch/xz2139/cosmo_dark/arrays/'+str(ID[0])+'_'+str(ID[1])+'_'+str(ID[2])+'.npy')
+        dm_mean=5.614541471004486
+        d_box=(d_box-dm_mean)/dm_mean
         f_box=np.load('/scratch/xz2139/cosmo_full/arrays/'+str(ID[0])+'_'+str(ID[1])+'_'+str(ID[2])+'.npy')
         return d_box,f_box
