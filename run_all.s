b97c2e8016677464e0ec9b01ea1bfc0c3a03a999
yanfangzero <zeroorez55@berkeley.edu>
2018-11-18 18:03:43 -0500

    krystal_fig

--- run_all.s
+++ run_all.s
@@ -7,17 +7,19 @@
 #SBATCH --mem=60GB
 #SBATCH --job-name=dark
 #SBATCH --mail-type=END
-#SBATCH --mail-user=bob.smith@nyu.edu
+#SBATCH --mail-user=yw1007@nyu.edu
 #SBATCH --output=slurm_%j.out
   
-#module purge
+module purge
 #module load tensorflow/python3.5/1.4.0 
 #imodule load cudnn/8.0v6.0
 #module load cuda/8.0.44
 #RUNDIR=$home/ys3202/dark/run-${SLURM_JOB_ID/.*}
 #mkdir -p $RUNDIR
+module load python3/intel/3.6.3
+source /home/yw1007/myenv/bin/activate
 modelidx=3
 lossweight=60
 yweight=10
-python src/main.py --lr 0.0001 --loss_weight $lossweight --model_idx $modelidx --epochs 12 --target_cat 'count' --target_class 0 --load_model 0 \
+python src1/main.py --lr 0.0001 --loss_weight $lossweight --model_idx $modelidx --epochs 12 --target_cat 'count' --target_class 0 --load_model 0 \
 --save_name "model_full_${modelidx}_${lossweight}_${yweight}" --record_results 0 --yfloss_weight $yweight > result_full_${modelidx}_${lossweight}_${yweight}.txt
