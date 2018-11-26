257996fffe2bafcbacc16a25463218374365680a
yanfangzero <zeroorez55@berkeley.edu>
2018-11-25 21:17:22 -0500

    merge conflict

--- src1/Models.py
+++ src1/Models.py
@@ -311,7 +311,6 @@ class R2CNN(nn.Module):
 		return x+x1
 
 class R2Unet(nn.Module):
-<<<<<<< HEAD
 	def __init__(self, in_channels, out_channels, t, reg = 0, sharpening = False):
 		super(R2Unet, self).__init__()
 		self.reg = reg
@@ -367,72 +366,6 @@ class R2Unet(nn.Module):
 			x14 = self.sharp_filter(x14)
 		return x14
 	
-=======
-    def __init__(self, in_channels, out_channels, t, reg = 0):
-        super(R2Unet, self).__init__()
-        self.reg = reg
-        self.in_ch= in_channels
-        self.out_ch = out_channels
-        self.t = t
-        self.avgPool = nn.AvgPool3d(2)
-        self.maxPool = nn.MaxPool3d(2)
-        self.r2cnn1 = R2CNN(self.in_ch, 32, 2)
-        self.r2cnn2 = R2CNN(32, 64, 2)
-        self.r2cnn3 = R2CNN(64, 128, 2)
-        self.r2cnn4 = R2CNN(128, 256, 2)
-        self.up_conv1 = self.up_conv_layer(256, 128, 3, 2, 1, 1)     
-        self.r2cnn5 = R2CNN(256, 64, 2)
-        self.up_conv2 = self.up_conv_layer(64, 64, 3, 2, 1, 1)
-        self.r2cnn6 = R2CNN(128, 32, 2)
-        self.up_conv3 = self.up_conv_layer(32, 32, 3, 2, 1, 1)
-        self.r2cnn7 = R2CNN(32, 16, 2)
-    
-        if self.reg == 0:
-            self.conv11 = nn.Conv3d(16, 2, kernel_size = 1, stride=1, padding=0)
-        else:
-            self.conv11 = nn.Conv3d(16, 1, kernel_size = 1, stride=1, padding=0)
-        
-    
-        self.gating_layer = UnetGating(256, 128).to(device)
-        
-        self.attn1 = GridAttentionBlock3D(in_channels=128, gating_channels=128)
-        self.attn2 = GridAttentionBlock3D(in_channels=64, gating_channels=128)
-        
-    def up_conv_layer(self, in_channels, out_channels, kernel_size, stride=3, padding=1, output_padding=1, bias=True):
-        layers = nn.Sequential(
-            nn.ConvTranspose3d(in_channels,out_channels, kernel_size=kernel_size, stride=stride, padding=padding,output_padding=output_padding, bias=True),
-            nn.ReLU()
-        )
-        return layers
-
-    def forward(self, x):
-        x1 = self.r2cnn1(x)    #32
-        x2 = self.maxPool(x1)  #32
-        x3 = self.r2cnn2(x2)   #64
-        x4 = self.maxPool(x3)  #64
-        x5 = self.r2cnn3(x4)   #128
-        x6 = self.maxPool(x5)  #128
-        x7 = self.r2cnn4(x6)   #256
-        gating = self.gating_layer(x7) #128 
-        x8 = self.up_conv1(x7) #128
-        #new
-        x5_atted, _ = self.attn1(x5, gating) #128
-        x8 = torch.cat((x5_atted, x8), dim = 1) #256
-        x9 = self.r2cnn5(x8)  #64
-        x10 = self.up_conv2(x9) #64
-        #new
-        x3_atted, _ = self.attn2(x3, gating) #64
-        x10 = torch.cat((x3_atted, x10), dim = 1) #128
-        x11 = self.r2cnn6(x10) #32
-        x12 = self.up_conv3(x11)
-        #x12 = torch.cat((x1, x12), dim = 1)
-        x13 = self.r2cnn7(x12)
-        x14 = self.conv11(x13)
-        if self.reg:
-            x14 = x14.squeeze(1)
-        return x14
-        
->>>>>>> 51f463e50b0f5819417e5441e4472d743f8540d0
 
 class one_layer_conv(nn.Module):
 	def __init__(self,in_channels, one_layer_outchannel, kernel_size, non_linearity, transformation, power):
